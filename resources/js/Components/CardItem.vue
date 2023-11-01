cb810e63d7d501cc6744a14b275bc1f924feb4e5
Shithappe <greenmad.b50@gmail.com>
2023-11-01 16:32:12 +0200

    main page: mobile view

--- resources/js/Components/CardItem.vue
+++ resources/js/Components/CardItem.vue
@@ -13,8 +13,8 @@ const images = ref(props.item.images.unshift(props.item.main_image));
 </script>
 
 <template>
-    <div class="w-full flex flex-col lg:flex-row gap-x-8 bg-gray-100 rounded-md">
-        <div class="w-2/3 lg:w-full rounded-lg shadow-lg">
+    <div class="w-full flex flex-col lg:flex-row mx-4 gap-x-8 bg-gray-100 rounded-md">
+        <div class="lg:w-full rounded-lg lg:shadow">
             <carousel id="gallery" :items-to-show="1" :wrap-around="false">
                 <slide v-for="image in item.images" :key="image">
                     <img class="object-cover w-full h-full rounded-lg" :src="image" alt="">
@@ -22,21 +22,26 @@ const images = ref(props.item.images.unshift(props.item.main_image));
 
                 <template #addons>
                     <navigation />
-                    <!-- <pagination /> -->
+                    <div class="lg:hidden">
+                        <pagination />
+                    </div>
                 </template>
             </carousel>
         </div>
 
 
         <div class="relative w-full lg:w-96 mx-6 py-6">
-            <div class="text-2xl font-bold">{{ item.price }} $</div>
             <div class="text-xl font-semibold">{{ item.builder_name }}</div>
-            <div class="mb-3">{{ item.city }}, {{ item.street }}</div>
+            <div>{{ item.city }}, {{ item.street }}</div>
 
-            <div class="my-1">{{ item.title }}</div>
+            <div class="mt-1 mb-4 flex gap-x-1">
+                <Lucide v-for="star in item.rate" class="w-6 h-6 fill-black" icon="Star" />
+            </div>
+
+            <div class="my-1 text-md">{{ item.title }}</div>
             <div>{{ item.description }}</div>
 
-            <div class="mt-4 mb-16 grid grid-cols-2 gap-y-2 font-medium justify-between">
+            <div class="mt-4 mb-6 grid grid-cols-2 gap-y-2 font-medium justify-between">
                 <div class="flex items-center gap-2">
                     <Lucide class="w-5 h-5" icon="BedDouble" /> {{ item.room_count }}
                 </div>
@@ -50,13 +55,12 @@ const images = ref(props.item.images.unshift(props.item.main_image));
                     <Lucide class="w-5 h-5" icon="BoxSelect" /> {{ item.price_per_meter }} per m²
                 </div>
             </div>
+            <div class="text-2xl font-bold mb-12">{{ item.price }} $</div>
 
-            <div >
 
-                <Link :href="'/estate/' + item.id">
-                    <button class="absolute bottom-6 max-w-full w-full p-3 text-md font-medium text-slate-100 bg-slate-900 rounded-lg">See Details</button>
-                </Link>
-            </div>
+            <Link :href="'/estate/' + item.id" class="absolute pr-12 lg:pr-0 bottom-4 w-full">
+                <button class="w-full p-3 text-md font-medium text-slate-100 bg-slate-900 rounded-lg">See Details</button>
+            </Link>
         </div>
     </div>
 </template>
