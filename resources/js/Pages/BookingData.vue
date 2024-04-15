cec997cd209d86bd17799451aa04d9ed00d4d79f
Shithappe <greenmad.b50@gmail.com>
2024-04-15 15:46:16 +0300

    history for filerTitle

--- resources/js/Pages/BookingData.vue
+++ resources/js/Pages/BookingData.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import SimpleAppLayout from '@/Layouts/SimpleAppLayout.vue';
 import Lucide from '@/Components/Lucide.vue';
@@ -30,7 +30,30 @@ const updateData = (newData) => {
     data.value = newData;
 }
 
+
+const history = ref(JSON.parse(localStorage.getItem('history')) || []);
+const showHistory = ref(false);
+
+const closeHistory = (event) => {
+  if (!event.target.closest('.history')) {
+    showHistory.value = false;
+  }
+};
+
 const applyFilters = async () => {
+    // add title to history
+    if (!history.value.includes(selectedTitle.value)) {
+        if (history.value.lenght >= 10) history.value.pop();
+        history.value.unshift(selectedTitle.value);
+        localStorage.setItem('history', JSON.stringify(history.value));
+    } else {
+        const index = history.value.findIndex(item => item === selectedTitle.value);
+            if (index !== -1) {
+                const selectedItem = history.value.splice(index, 1)[0];
+                history.value.unshift(selectedItem);
+            }
+    }
+
     localStorage.setItem('selectedTitle', selectedTitle.value)
     useFilters.value = true;
     load.value = true;
@@ -55,25 +78,22 @@ const applyFilters = async () => {
 const closeFilters = () => {
     showFilters.value = false;
 }
+
+onMounted(() => {
+  document.addEventListener('click', closeHistory);
+});
 </script>
 
 <template>
     <SimpleAppLayout title="Головна">
 
-        <!-- :cities="props.cities"  -->
-            <SideBarFilters 
-                :show="showFilters"
-                :countries="props.countries"
-                :types="props.types" 
-                :facilities="props.facilities" 
-                @applyFilters="applyFilters" 
-                @closeFilters="closeFilters"
-            />
+        <SideBarFilters :show="showFilters" :countries="props.countries" :types="props.types"
+            :facilities="props.facilities" @applyFilters="applyFilters" @closeFilters="closeFilters" />
 
-        <div class="w-full px-4 py-6 mx-auto" :class="{ 'lg:w-4/5 lg:float-right lg:pl-24': showFilters, 'lg:px-24 lg:max-w-8xl': !showFilters }">
+        <div class="w-full px-4 py-6 mx-auto"
+            :class="{ 'lg:w-4/5 lg:float-right lg:pl-24': showFilters, 'lg:px-24 lg:max-w-8xl': !showFilters }">
 
-            <div class="flex ml-4"
-                :class="{ 'xl:pl-4 2xl:pl-10': showFilters }">
+            <div class="flex ml-4" :class="{ 'xl:pl-4 2xl:pl-10': showFilters }">
                 <button
                     class="px-2 py-0 rounded-lg shadow hover:shadow-lg hover:text-slate-100 hover:bg-black appearance-none leading-5 transition duration-300 ease-in-out text-md"
                     :class="{ 'shadow-lg text-slate-100 bg-black': showFilters }"
@@ -81,30 +101,31 @@ const closeFilters = () => {
                     <Lucide icon="Filter" />
                 </button>
 
-                <div class="relative w-full lg:max-w-4xl flex mx-4 transition duration-150 ease-in-out">
+                <div class="history relative w-full lg:max-w-4xl flex mx-4 transition duration-150 ease-in-out">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none">
                         <Lucide icon="Search" />
                     </div>
-                    <input 
+                    <input
                         ref="searchInput"
-                        type="search" 
-                        v-model="selectedTitle" 
+                        type="search"
+                        v-model="selectedTitle"
                         @keyup.enter="applyFilters"
+                        @focus="showHistory = true"
                         class="block w-full p-2 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 transition duration-300 ease-in-out"
                         placeholder="Komaneka at Bisma..." required>
-                    <button  @click="applyFilters"
+                    <button @click="applyFilters"
                         class="text-white absolute end-px inset-y-px bg-blue-700 focus:outline-none font-medium rounded-lg text-md px-4 py-2">Search</button>
+                    <ul v-if="history.length > 0 && showHistory" @blur="showHistory = false" class="history absolute bg-white mt-12 w-full rounded-md shadow-lg z-10">
+                        <li v-for="(item, index) in history" :key="index" class="p-2 hover:bg-gray-100 cursor-pointer" @click="() => {selectedTitle = item; showHistory = false; applyFilters()}">{{ item }}</li>
+                    </ul>
                 </div>
 
             </div>
 
-            <div 
-                class="my-8 flex flex-wrap" 
+            <div class="my-8 flex flex-wrap"
                 :class="{ 'xl:pl-4 2xl:pl-10': showFilters, 'justify-center': !showFilters, 'opacity-50': load }">
                 <CardBookingData v-for="item in data.data" :key="item.id" :item="item" class="col-span-1" />
             </div>
-
-            <!-- {{ data.data }} -->
 
             <PaginationPost v-if="useFilters" class="mt-6" :links="data.links" :updateData="updateData" />
             <Pagination v-else class="mt-6" :links="data.links" />
