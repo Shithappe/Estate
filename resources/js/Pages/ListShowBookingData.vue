18e06d36927d215f6b88c055f5f3ba1abf982cff
Shithappe <greenmad.b50@gmail.com>
2024-11-05 13:58:46 +0200

    add Copy Link to list

--- resources/js/Pages/ListShowBookingData.vue
+++ resources/js/Pages/ListShowBookingData.vue
@@ -95,6 +95,8 @@ watch(dateRange, (newDateRange) => {
 });
 
 onMounted(() => {
+    console.log(props.list);
+
     // Устанавливаем начальный диапазон дат: от месяца назад до сегодня
     const endDate = moment();
     const startDate = moment().subtract(1, 'month');
@@ -119,6 +121,32 @@ const filteredBookingData = computed(() => {
     ).filter(filteredRooms => filteredRooms.length > 0);
 });
 
+const copied = ref(false);
+const copyLink = (click = false) => {
+    setAccessLink(props.list);
+
+    const shareLink = `${window.location.origin}/list/share/${props.list.share_token}`;
+    if (click) {
+        navigator.clipboard.writeText(shareLink);
+        copied.value = true;
+        setTimeout(() => {
+            copied.value = false;
+        }, 1000);
+    }
+    return shareLink;
+}
+
+const setAccessLink = async (item) => {
+    try {
+        await axios.patch(`/api/list/${item.id}`, {
+            user_id: props.auth.user.id,
+            privacy_mode: "link"
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 const removeItemFromList = (listType, itemId) => {
     // console.log(listType, itemId);
 
@@ -142,19 +170,36 @@ const removeItemFromList = (listType, itemId) => {
 
                 <div class="flex justify-between items-center gap-x-2 mb-4">
                     <span class="text-2xl font-medium">{{ props.list.name }}</span>
-                    <Dropdown align="right" width="48" :contentClasses="['py-1', 'bg-white']">
-                        <template #trigger>
-                            <button class="flex px-2 py-1 rounded-lg shadow hover:bg-gray-200">
-                                <span>{{ selectedOption }}</span>
-                                <Lucide class="w-5 h-5 mt-1" icon="ChevronDown" />
-                            </button>
-                        </template>
-                        <template #content>
-                            <div v-if="props.list.type === 'complex'" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Complexes')">Complexes</div>
-                            <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Units')">Units</div>
-                            <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Map')">Map</div>
-                        </template>
-                    </Dropdown>
+                    <div class="flex gap-x-2">
+                        <button @click="copyLink(true)" data-copy-to-clipboard-target="npm-install-copy-text" class="text-gray-900 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border">
+                            <span v-if="!copied" id="default-message" class="inline-flex items-center">
+                                <svg class="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                    <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
+                                </svg>
+                                <span class="text-xs font-semibold">Copy Link</span>
+                            </span>
+                            <span v-else id="success-message" class="inline-flex items-center">
+                                <svg class="w-3 h-3 text-blue-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                                </svg>
+                                <span class="text-xs font-semibold text-blue-700">Copied</span>   
+                            </span>
+                        </button>
+
+                        <Dropdown align="right" width="48" :contentClasses="['py-1', 'bg-white']">
+                            <template #trigger>
+                                <button class="flex px-2 py-1 rounded-lg shadow hover:bg-gray-200">
+                                    <span>{{ selectedOption }}</span>
+                                    <Lucide class="w-5 h-5 mt-1" icon="ChevronDown" />
+                                </button>
+                            </template>
+                            <template #content>
+                                <div v-if="props.list.type === 'complex'" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Complexes')">Complexes</div>
+                                <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Units')">Units</div>
+                                <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click="selectOption('Map')">Map</div>
+                            </template>
+                        </Dropdown>
+                    </div>
                 </div>
 
                 <div v-if="selectedOption == 'Complexes'" class="my-8 flex flex-wrap">
